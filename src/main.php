fbf8e2a0c398aaeb90db9b596418f5609f23f04b
Diego Ramirez <diego.ramirez@altatec.com.mx>
2022-05-29 23:34:06 -0500

    añnadido try catch para evitar que truene el programa si no encuentra la url

--- src/main.php
+++ src/main.php
@@ -11,8 +11,12 @@ date_default_timezone_set('America/Mexico_City');
 $program = new SMNClient();
 
 while(true){
-
-    $program->start();
+    
+    try{
+        $program->start();
+    }catch(Exception $e){
+        echo "Error al consultar el clima: " . $e->getMessage() - " \n";
+    }
     sleep(5*60);
 
 }
